dbec921aa1faf955ff3785e917903395f22b6770
jackray <jackray_wang@sina.com>
2018-12-16 17:22:13 +0800

    springboot+generator+mybatis+log4j2

---
+++ src/main/java/com/jakcray/springbootshiro/SpringbootshiroApplication.java
@@ -0,0 +1,14 @@
+package com.jakcray.springbootshiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootshiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootshiroApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/jakcray/springbootshiro/controller/TestController.java
@@ -0,0 +1,36 @@
+package com.jakcray.springbootshiro.controller;
+
+import com.jakcray.springbootshiro.mybatisutils.dao.UserDAO;
+import com.jakcray.springbootshiro.mybatisutils.mapper.UserDAOMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    UserDAOMapper userDAOMapper;
+
+
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class.getName());
+
+    @RequestMapping(value = "/test",method = RequestMethod.GET)
+    public String Test(){
+
+        UserDAO userDAO = userDAOMapper.selectByPrimaryKey("12345");
+
+        logger.info("success");
+
+
+//        System.out.println(userDAO.getEmail());
+//
+//        System.out.println("success");
+
+        return "big success";
+    }
+
+}
